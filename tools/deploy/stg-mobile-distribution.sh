5b26fd39dd3ca73b5d4187e619d792e1a3e72f99
Raviteja <ravi@mesha.sg>
2026-08-19 10:50:03 +0530

    Use exact STG lint skip for mobile deploy

--- tools/deploy/stg-mobile-distribution.sh
+++ tools/deploy/stg-mobile-distribution.sh
@@ -230,7 +230,6 @@ DEPLOY_VERSION_NAME="${GOATOS_ANDROID_VERSION_NAME:-}"
 
 cd apps/goatos-android
 common_gradle_args=(
-  -x lintVitalRelease \
   -x lintVitalStgRelease \
   -x lintVitalAnalyzeRelease \
   -x lintVitalAnalyzeStgRelease \
